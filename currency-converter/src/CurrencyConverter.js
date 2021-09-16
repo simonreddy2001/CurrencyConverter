f9c753ab79fe6289e7cdc34359d2f8016a7f042a
simonreddy2001 <simon.reddy2001@gmail.com>
2021-09-16 11:24:04 +0200

    update3

--- currency-converter/src/CurrencyConverter.js
+++ currency-converter/src/CurrencyConverter.js
@@ -1,18 +1,19 @@
 import { useEffect, useState } from "react";
 
 const defaultCurrency = "USD";
-
+const defaultChangingCurrency = "DKK";
 const CurrencyConverterApp = () => {
     const [selectedCurrency, setSelectedCurrency] = useState(defaultCurrency);
+    const [selectedChangingCurrency, setSelectedChangingCurrency] = useState(defaultChangingCurrency);
     const [convertedCurrency, setConvertedCurrency] = useState(0);
 
     useEffect(() => {
         fetch(`https://open.er-api.com/v6/latest/${selectedCurrency}`)
             .then((response) => response.json())
             .then((result) => {
-                setConvertedCurrency(result.rates.DKK);
+                setConvertedCurrency(result.rates[selectedChangingCurrency]);
             });
-    }, [selectedCurrency]);
+    }, [selectedCurrency, selectedChangingCurrency]);
 
     return (
         <div>
@@ -20,9 +21,16 @@ const CurrencyConverterApp = () => {
                 <option value="USD">USD</option>
                 <option value="EUR">EUR</option>
                 <option value="CHF">CHF</option>
+                <option value="DKK">DKK</option>
+            </select>
+            <select onChange={(event) => setSelectedChangingCurrency(event.target.value)}>
+                <option value="DKK">DKK</option>
+                <option value="USD">USD</option>
+                <option value="EUR">EUR</option>
+                <option value="CHF">CHF</option>
             </select>
             <h1>
-                1 {selectedCurrency} = {convertedCurrency} DKK
+                1 {selectedCurrency} = {convertedCurrency} {selectedChangingCurrency}
             </h1>
         </div>
     );
